792d8b6a65e59e826235fb11b22740a49a9a6a03
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-30 21:56:16 +0530

    Added : initialized the user registration repository

---
+++ src/com/bridgelabz/userregistration/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.userregistration;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("!!! welcome to user registration validation problem !!!");
+    }
+
+}
